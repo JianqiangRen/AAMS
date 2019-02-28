4dc966dd7c27f2bf0dd6c113ae43352389914dd3
rjq <jianqiang.rjq@alibaba-inc.com>
2019-02-28 14:38:47 +0800

    add pre-trained model

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-python test.py --model tfmodel/aams.pb \
+python test.py --model tf_model/aams.pb \
                         --content images/content/lenna_cropped.jpg \
                         --style images/style/candy.jpg \
                         --inter_weight 1.0
